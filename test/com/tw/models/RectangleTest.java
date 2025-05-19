d42b1c76bf2c0d724d5f2bd4c6f213b5130a186f
AnaghPaul <125043274+AnaghPaul@users.noreply.github.com>
2025-05-19 11:23:43 +0530

    Adds test cases

--- test/com/tw/models/RectangleTest.java
+++ test/com/tw/models/RectangleTest.java
@@ -8,13 +8,17 @@ class RectangleTest {
     @Test
 
     void calculatesArea() {
-        Rectangle rectangle = new Rectangle(1, 2);
-        assertEquals(2,rectangle.area(),"area of rectangle should be 2 for dimensions 1 X 2");
+        assertEquals(2,new Rectangle(1, 2).area(),"area of rectangle should be 2 for dimensions 1 X 2");
+        assertEquals(0,new Rectangle(1, 0).area(),"area of rectangle should be 0 for dimensions 1 X 0");
+        assertEquals(0,new Rectangle(0, 1).area(),"area of rectangle should be 0 for dimensions 0 X 1");
+        assertEquals(0,new Rectangle(0, 0).area(),"area of rectangle should be 0 for dimensions 0 X 0");
     }
 
     @Test
     void calculatesPerimeter() {
-        Rectangle rectangle = new Rectangle(1, 2);
-        assertEquals(6,rectangle.perimeter(),"perimeter of rectangle of dimensions 1 X 2 should be 6");
+        assertEquals(6,new Rectangle(1, 2).perimeter(),"perimeter of rectangle of dimensions 1 X 2 should be 6");
+        assertEquals(2,new Rectangle(1, 0).perimeter(),"perimeter of rectangle of dimensions 1 X 0 should be 2");
+        assertEquals(2,new Rectangle(0, 1).perimeter(),"perimeter of rectangle of dimensions 0 X 1 should be 2");
+        assertEquals(0,new Rectangle(0, 0).perimeter(),"perimeter of rectangle of dimensions 0 X 0 should be 0");
     }
 }
--- test/com/tw/models/SquareTest.java
+++ test/com/tw/models/SquareTest.java
@@ -7,13 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class SquareTest {
     @Test
     void calculatesArea() {
-        Square square = new Square(2);
-        assertEquals(4,square.area(),"area of square with side 2 should be 4");
+        assertEquals(4,new Square(2).area(),"area of square with side 2 should be 4");
+        assertEquals(0,new Square(0).area(),"area of square with side 0 should be 0");
     }
 
     @Test
     void calculatesPerimeter() {
-        Square square = new Square(2);
-        assertEquals(8,square.perimeter(),"perimeter of square with side 2 should be 8");
+        assertEquals(8,new Square(2).perimeter(),"perimeter of square with side 2 should be 8");
+        assertEquals(0,new Square(0).perimeter(),"perimeter of square with side 0 should be 0");
     }
 }
